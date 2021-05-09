05012f2f5e965f17583da53f9dfc1a0257d3d559
thotran2015 <thotran9@mit.edu>
2021-05-09 00:38:42 -0400

    resizing filechooser

--- testing_filechooser.py
+++ testing_filechooser.py
@@ -19,13 +19,21 @@ class FileSelector(GridLayout):
         file_upload_btn = Button(text='Choose a file', font_size=24, size_hint_y=0.5)
 
         popup_layout = GridLayout(cols=1)
-        self.file_chooser = FileChooserListView(path='/home/tho/Dropbox (MIT)/MIT_IHTFP/MEng/6.835/FP/midi_files/')
+        self.file_chooser = FileChooserListView(path='/home/tho/Dropbox (MIT)/MIT_IHTFP/MEng/6.835/FP/midi_files/',
+                                                on_entry_added=self.update_file_list_entry,
+                                                on_subentry_to_entry=self.update_file_list_entry)
+
+        boxlayout_children = self.file_chooser.children[0].children[0].children[-1].children
+        for child in boxlayout_children:
+            if isinstance(child, Label):
+                child.font_size = 24
+
         # create content and add to the popup
         close_button = Button(text='Done!', font_size=24, size_hint_y=0.2)
 
         popup_layout.add_widget(self.file_chooser)
         popup_layout.add_widget(close_button)
-        self.popup = Popup(title='Directories', title_size=18,
+        self.popup = Popup(title='Directories', title_size=26,
                       content=popup_layout,)
                       #size_hint=(None, None), size=(500, 500))
         close_button.bind(on_press=self.off_button_press)
@@ -33,6 +41,12 @@ class FileSelector(GridLayout):
 
         self.overview_layout.add_widget(self.placeholder_file)
         self.overview_layout.add_widget(file_upload_btn)
+
+    def update_file_list_entry(self, file_chooser, file_list_entry, *args):
+        file_list_entry.ids['filename'].font_size = 24
+        size_label = file_list_entry.children[0].children[0]
+        size_label.font_size = 24
+
 
     def off_button_press(self, *args):
         if len(self.file_chooser.selection) > 0:
